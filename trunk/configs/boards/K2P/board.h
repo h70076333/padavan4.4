9b88433c394c76848a1ecdf3d41f2360f82bce55
h70076333 <h70076222@88.com>
2024-08-31 17:09:15 +0800

    Update board.h

--- trunk/configs/boards/K2P/board.h
+++ trunk/configs/boards/K2P/board.h
@@ -1,19 +1,19 @@
-/* K2P */
+/* KN18N */
 
-#define BOARD_PID		"K2P"
-#define BOARD_NAME		"K2P"
-#define BOARD_DESC		"PHICOMM K2P Wireless Router"
-#define BOARD_VENDOR_NAME	"PHICOMM"
-#define BOARD_VENDOR_URL	"http://www.phicomm.com/"
-#define BOARD_MODEL_URL		"http://www.phicomm.com/"
+#define BOARD_PID		"KN18N"
+#define BOARD_NAME		"KN18N"
+#define BOARD_DESC		"H3C KN18N Wireless Router"
+#define BOARD_VENDOR_NAME	"H3C"
+#define BOARD_VENDOR_URL	"http://www.h3c.com/"
+#define BOARD_MODEL_URL		"http://www.h3c.com/"
 #define BOARD_BOOT_TIME		30
 #define BOARD_FLASH_TIME	120
 
-#define BOARD_HAS_5G_11AC		1
-#define BOARD_NUM_ANT_5G_TX		2
-#define BOARD_NUM_ANT_5G_RX		2
-#define BOARD_NUM_ANT_2G_TX		2
-#define BOARD_NUM_ANT_2G_RX		2
+#define BOARD_HAS_5G_11AC		0
+#define BOARD_NUM_ANT_5G_TX		0
+#define BOARD_NUM_ANT_5G_RX		0
+#define BOARD_NUM_ANT_2G_TX		0
+#define BOARD_NUM_ANT_2G_RX		0
 #define BOARD_HAS_EPHY_L1000	1
 #define BOARD_HAS_EPHY_W1000	1
 #define BOARD_NUM_ETH_LEDS		0
